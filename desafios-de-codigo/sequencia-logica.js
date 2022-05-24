038c547ae2c2100be47e45168b95b14311cfc29b
liviaalvzs <liv.alvessantana@gmail.com>
2022-05-24 16:55:50 -0300

    desafio 'sequencia-logica' concluido (alteracao no console.log)

--- desafios-de-codigo/sequencia-logica.js
+++ desafios-de-codigo/sequencia-logica.js
@@ -6,6 +6,6 @@ let lines = gets().split("\n");
 let n = parseInt(lines.shift());
 
 for (let i = 1; i <= n; i++) {
-  print(`${i} ${i ** 2} ${i ** 3}`);
-  print(`${i} ${i ** 2 +  1 } ${i ** 3 + 1}`);
+  console.log(`${i} ${i ** 2} ${i ** 3}`);
+  console.log(`${i} ${i ** 2 +  1 } ${i ** 3 + 1}`);
 }
